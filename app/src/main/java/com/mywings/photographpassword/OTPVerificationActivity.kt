60a8466c280e7ded3e45d671f52d93381a7e097c
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2018-02-26 06:50:48 +0530

    added verification screen

--- app/src/main/java/com/mywings/photographpassword/OTPVerificationActivity.kt
+++ app/src/main/java/com/mywings/photographpassword/OTPVerificationActivity.kt
@@ -1,12 +1,35 @@
 package com.mywings.photographpassword
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_otpverification.*
+
 
 class OTPVerificationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otpverification)
+
+
+        btnVerify.setOnClickListener {
+
+            if (txtOtp.text.isNotEmpty() && txtOtp.text.toString().contentEquals("000000")) {
+                val intent = Intent(this@OTPVerificationActivity, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                show("Enter valid otp")
+            }
+
+
+        }
+
+
+    }
+
+    private fun show(message: String) {
+        Snackbar.make(btnVerify, message, Snackbar.LENGTH_LONG).show()
     }
 }
